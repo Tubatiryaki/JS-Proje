392a40f2ad7fbc7bd8b5de9cb544e990d9dc88f8
Tubatiryaki <tubatiryaki25@gmail.com>
2023-08-16 18:00:38 +0300

    16.08.2023

---
+++ sablon/script.js
@@ -0,0 +1,14 @@
+
+
+
+function updateLogin() {
+    
+    const name=document.getElementById("name");
+    
+    document.getElementById("sonuc").innerText= `Hoş Geldiniz ${name.value}`;
+
+   name.value="";
+   password.value="";
+   
+}
+// document.getElementById("button").addEventListener("click");
